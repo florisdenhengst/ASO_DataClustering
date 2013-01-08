e79b49b71b78a817742bea2bb810939a9b4aea69
thombohlk <thomasbohlken@gmail.com>
2013-01-08 14:39:07 +0100

    Added new ant clustering algorithm based on imported opinions which are classified as a property.

---
+++ AntClustering-property-based.py
@@ -0,0 +1,325 @@
+import math
+import random
+import time
+import urllib2
+import os.path
+import re
+import sys
+from Tkinter import *
+from xml.dom.minidom import parseString
+
+datasetSize = 1200		# Determine later
+dropThreshold = 0.01		# Determine later
+pickupThreshold = 0.5	# Determine later
+
+pickupConst = 1
+dropConst = 1
+alpha = 100
+
+bLoop = False
+bAllAnts = True
+speed = 0.1
+modSpeed = 1
+generation = 0
+
+# Classes
+class Ant:
+	def __init__(self, x, y, load = None):
+		self.x = x
+		self.y = y
+		self.load = load
+
+class DataItem:
+	def __init__(self, x, y, data = []):
+		self.x = x
+		self.y = y
+		self.data = data
+		
+class Subject(object):
+    def __init__(self, sub=None, polarity=None):
+        self.sub = sub
+        self.polarity = polarity
+
+# Chance an ant picks up the item on its position: ( Kp / ( Kp + F(i) )^2
+def pickupChance(ant):
+	return math.pow((pickupConst / (pickupConst + localSimilarity(ant))), 2)
+	
+# Chance an ant drops an item on its current position:
+#	if F(i) < Kd	2F(i)
+#	else				1
+def dropChance(ant):
+	locSim = localSimilarity(ant)
+	if locSim < dropConst:
+		return 2 * locSim
+	return 1
+
+# Calculate local similarity
+def localSimilarity(ant):
+	locality = 1 / math.pow(localDist * 2, 2)
+	locSim = 0;
+	for dataItem in dataItems:
+		if inLocalArea(ant, dataItem):
+			locSim += (1 - similarity(ant, dataItem) / alpha)
+	
+	result = locality * locSim;
+	return max(result, 0)
+
+# Determine if one ant is in local area of the other
+def inLocalArea(ant, dataItem):
+	if abs(ant.x - dataItem.x) <= localDist and abs(ant.y - dataItem.y) <= localDist:
+		return True 
+	return False
+
+# Distance between to items
+def similarity(ant, dataItem):
+	diff = 0
+	overlap = False
+	if ant.load:
+		for iItem in ant.load.data:
+			for jItem in dataItem.data:
+				# TODO: worden hier items dubbel geteld?
+				if iItem.sub == jItem.sub:
+					overlap = True
+					diff += abs(int(iItem.polarity) - int(jItem.polarity))
+	
+	if overlap:
+		return min(1, diff/10)
+	else:
+		return 1
+
+# Creates ants and places them randomly on grid
+def createAnts(nrOfAnts):
+	for i in range(1, nrOfAnts):
+		antColony.append(Ant(random.randint(0, gridUpperXBound), random.randint(0, gridUpperYBound)))
+	return
+
+def loadDataItems():
+	# TODO, depends on data
+	return
+
+def itemOnLocation(ant):
+	for item in dataItems:
+		if item.x == ant.x and item.y == ant.y:
+			return item
+	return False
+
+def iterateAnt(ant):
+	if ant.load is not None:
+		d = dropChance(ant)
+		if d > dropThreshold:
+			dropItem(ant)
+	else:
+		item = itemOnLocation(ant)
+		if item is not False and pickupChance(ant) > pickupThreshold:
+			pickupItem(ant, item)
+	
+	# Move ant in random direction
+	moveAnt(ant)
+	return
+	
+def dropItem(ant):
+	ant.load.x = ant.x
+	ant.load.y = ant.y
+	ant.load = None
+	return
+	
+def pickupItem(ant, item):
+	ant.load = item
+	return
+
+def moveAnt(ant):
+	chance = random.random()
+	if chance > 0.75:
+		ant.x += 1
+	elif chance > 0.5:
+		ant.x -= 1
+	elif chance > 0.25:
+		ant.y += 1
+	else:
+		ant.y -= 1
+	if ant.x > gridUpperXBound:
+		ant.x = 0
+	if ant.x < gridLowerXBound:
+		ant.x = gridUpperXBound
+	if ant.y > gridUpperYBound:
+		ant.y = 0
+	if ant.y < gridLowerYBound:
+		ant.y = gridUpperYBound
+	if ant.load is not None:
+		ant.load.x = ant.x
+		ant.load.y = ant.y
+	return
+
+def drawAnts():
+	canvas.delete("all")
+	for ant in antColony:
+		canvas.create_oval(ant.x-3, ant.y-3, ant.x+3, ant.y+3, fill="#805555")
+	for dataItem in dataItems:
+		canvas.create_oval(dataItem.x-3, dataItem.y-3, dataItem.x+3, dataItem.y+3, fill="#fff")
+	canvas.update()
+	return
+
+def setSpeed():
+	global speed
+	global sSpeed
+	
+	speed = speed/10
+	if speed < 0.0001:
+		speed = 1.0
+	sSpeed.set(str(1/speed)+" ant (colony) updates/s")
+	return
+	
+def setModSpeed():
+	global modSpeed
+	global sModSpeed
+	modSpeed *= 10
+	if modSpeed > 1000:
+		modSpeed = 1
+	sModSpeed.set(value="Canvas updated after "+str(modSpeed)+" generations")
+	return
+	
+def setPause():
+	global bLoop
+	global sPlay
+	if bLoop == True:
+		bLoop = False
+		sPlay.set("Play")
+	else:
+		bLoop = True
+		sPlay.set("Pause")
+	return
+	
+def setAllAnts():
+	global bAllAnts
+	global sAll
+	if bAllAnts == True:
+		bAllAnts = False
+		sAll.set("Update one ant")
+	else:
+		bAllAnts = True
+		sAll.set("Update all ants")
+	return
+
+# Create 2D grid which has a surface of 10N: 10 * sqrt(N) by 10 * sqrt(N)
+gridLowerXBound = 0
+gridLowerYBound = 0
+gridUpperXBound = int(10 * math.sqrt(datasetSize))
+gridUpperYBound = int(10 * math.sqrt(datasetSize))
+
+# Define local area size
+localDist = 20			#Determine later
+
+#Create N/10 number of ants and place randomly in grid
+antColony = []
+createAnts(datasetSize/10)
+
+# Load data and place data items randomly in the grid
+dataItems = []
+loadDataItems()
+
+# Draw main canvas
+root = Tk()
+root.title("Incredibly realistic ant colony")
+root.geometry(str(gridUpperXBound)+"x"+str(gridUpperYBound+150)+"+100+100")
+canvas = Canvas(root, width=str(gridUpperXBound), height=str(gridUpperYBound), bg='#40DE58')
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Draw buttons
+sPlay = StringVar(value="Play")
+butPlay = Button(root, textvariable=sPlay, command=setPause)
+butPlay.grid(row=1, column=0, sticky=E)
+
+sGeneration = StringVar(value="Generation = "+str(generation))
+lGeneration = Label(root, textvariable=sGeneration)
+lGeneration.grid(row=1, column=1, sticky=W)
+
+butSpeed = Button(root, text="Speed", command=setSpeed)
+butSpeed.grid(row=2, column=0, sticky=E)
+sSpeed = StringVar(value=str(1/speed)+" ant (colony) updates/s")
+lSpeed = Label(root, textvariable=sSpeed)
+lSpeed.grid(row=2, column=1, sticky=W)
+
+butModSpeed = Button(root, text="Fast forward", command=setModSpeed)
+butModSpeed.grid(row=3, column=0, sticky=E)
+sModSpeed = StringVar(value="Canvas updated after "+str(modSpeed)+" generations")
+lModSpeed = Label(root, textvariable=sModSpeed)
+lModSpeed.grid(row=3, column=1, sticky=W)
+
+sAll = StringVar(value="Update all ants")
+butAll = Button(root, textvariable=sAll, command=setAllAnts)
+butAll.grid(row=4, column=0, columnspan=2)
+
+### Process data ###
+hotel = 1
+review = 1
+subjects = []
+
+subjects = []
+
+print "Loading data..."
+ 
+while hotel < 500:
+	filename = 'KAF/review-'+str(hotel)+"-"+str(review)+'.xml'
+	if os.path.exists(filename):
+		
+		file = open(os.path.abspath('KAF/review-'+str(hotel)+"-"+str(review)+'.xml'), 'r')
+		data = file.read()
+		file.close()
+		
+		dom = parseString(data)
+		properties = dom.getElementsByTagName('property')
+		opinions = dom.getElementsByTagName('opinion')
+		
+		for opinion in opinions:
+			oTargetTag = opinion.getElementsByTagName('target')[0]
+			oTarget = oTargetTag.attributes['id'].value
+			oExpressionTag = opinion.getElementsByTagName('opinion_expression')[0]
+			polarity = oExpressionTag.attributes['strength'].value
+			
+			for prop in properties:
+				pTargetTags = prop.getElementsByTagName('target')
+				for pTargetTag in pTargetTags:
+					if pTargetTag.attributes['id'].value == oTarget:
+						pSubject = prop.attributes['type'].value
+						unique = True
+						for subject in subjects:
+							if subject.sub == pSubject:
+								unique = False
+						
+						if unique == True:
+							subjects.append(Subject(pSubject, polarity))
+		
+		
+		review += 1
+	else:
+		dataItems.append(DataItem(random.randint(0, gridUpperXBound), random.randint(0, gridUpperYBound), subjects))
+		subjects = []
+		hotel += 1
+		review = 1
+
+### Ant clustering ###
+while 1:
+	# Do one generation if not paused
+	if bLoop:
+		generation += 1
+		sGeneration.set("Generation = "+str(generation))
+		if bAllAnts:
+			# Iterate all ants
+			for ant in antColony:
+				iterateAnt(ant)
+		else:
+			# Select a random ant and iterate
+			ant = random.choice(antColony)
+			iterateAnt(ant)
+		
+		# Every modSpeed-steps the canvas is drawn(for FastForwarding)
+		if (generation%modSpeed == 0):
+			drawAnts()
+		# Sleep 'speed'-time units to make the speed variable
+		time.sleep(speed)
+	
+	# Update the canvas each loop to make sure the buttons still work
+	canvas.update()
+
+root.mainloop()
+
